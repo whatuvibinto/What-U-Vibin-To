e33f373e9ae27a2dcbf19a67c0f13f834fd32d3e
Lucy <91936727+whatuvibinto@users.noreply.github.com>
2026-01-11 14:29:24 +0900

    Update dig_data.js

--- js/dig_data.js
+++ js/dig_data.js
@@ -8,24 +8,12 @@ window.digData = [
         role: "", 
         date: "2026-01-10",
         location: "TOKYO",
-        genre: ["DJ MIX", "HIPHOP", "R&B"], // DJ MIXを含める
+        genre: ["HIPHOP", "R&B", "DUB"],
         image: "images/lucy/lucy.png", 
         qr: "images/lucy/lucy_qr.png",
         serviceIcon: "images/SpotifyLogo.png",
         qrLink: "friends/lucy.html",
         instagram: "yng.r"
-    },
-    {
-        name: "Hiraku Tajima",
-        role: "DJ",
-        date: "2026-01-11",
-        location: "SOMEWHERE IN JAPAN",
-        genre: ["JAZZ", "SOUL"],
-        image: "images/hiraku_tajima/hiraku_tajima.png",
-        qr: "images/hiraku_tajima/hiraku_tajima_qr.png",
-        serviceIcon: "images/AppleMusicLogo.png",
-        qrLink: "friends/hiraku_tajima.html",
-        instagram: "udoneatbeat"
     }
     // ここに新しいデータを追加していく形式です
 ];
