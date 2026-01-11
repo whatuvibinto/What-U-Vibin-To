df21362858447615e7910f43d3e1d8243a23f605
Lucy <91936727+whatuvibinto@users.noreply.github.com>
2026-01-11 12:48:27 +0900

    Update dig_data.js

--- js/dig_data.js
+++ js/dig_data.js
@@ -8,10 +8,10 @@ const digData = [
         date: "2026-01-10",
         location: "TOKYO",
         genre: ["HIPHOP","R&B","DUB"], 
-        image: "images/Lucy/lucy.png", 
-        qr: "images/Lucy/lucy_qr.png",
+        image: "images/lucy/lucy.png", 
+        qr: "images/lucy/lucy_qr.png",
         serviceIcon: "images/SpotifyLogo.png",
-        qrLink: "Friends/Lucy.html",
+        qrLink: "friends/lucy.html",
         instagram: "yng.r" // ★追加: インスタID
     },
     {
@@ -20,10 +20,10 @@ const digData = [
         date: "2026-01-11",
         location: "SOMEWHERE IN JAPAN",
         genre: ["JAZZ", "SOUL"],
-        image: "images/Hiraku tajima/Hiraku Tajima.png",
-        qr: "images/HIraku Tajima/Hiraku Tajima.png",
+        image: "images/hiraku_tajima/hiraku_tajima.png",
+        qr: "images/hiraku_tajima/hiraku_tajima_qr.png",
         serviceIcon: "images/AppleMusicLogo.png",
-        qrLink: "https://music.apple.com/jp/playlist/xxxxxx",
+        qrLink: "friends/hiraku_tajima.html",
         instagram: "udoneatbeat" // ★追加
     },
    
